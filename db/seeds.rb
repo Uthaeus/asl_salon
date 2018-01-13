d3b4430bab86367c8df1ebc06be9ff9d68b05063
Uthaeus <Uth@github.com>
2018-01-12 22:50:48 -0700

    Adjusted nav links and created seed data for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+8.times do |product_item|
+  Product.create!(
+        title: "Product title: #{product_item}",
+        brand: "The Bestest",
+        price: 35.00,
+        image: "http://via.placeholder.com/150x200"
+  )
+end
+
+puts "8 products created"
+
+User.create!(email: "test@test.com", name: "Jim", password: "asdfasdf", password_confirmation: "asdfasdf")
+
+puts "1 User created"
+
+
+
+
+
+
